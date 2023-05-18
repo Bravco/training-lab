5e5a064216ef886c9b1382dbed4a492dd74a7354
Bravco <boss.wiw@gmail.com>
2023-05-18 17:33:16 +0200

    Added hidden firebase config file

--- web/src/firebase/index.js
+++ web/src/firebase/index.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, collection, doc } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBJ-TcKnIH2-yoPPjP1bylDHY_CJyJ0OSo",
-    authDomain: "trading-lab-eed4c.firebaseapp.com",
-    projectId: "trading-lab-eed4c",
-    storageBucket: "trading-lab-eed4c.appspot.com",
-    messagingSenderId: "464147513286",
-    appId: "1:464147513286:web:9fe1b9828682676d087cc7",
-    measurementId: "G-8F3W4DH407"
-};
+import { firebaseConfig } from "./config.js";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 
