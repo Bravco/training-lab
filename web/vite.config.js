7f12e43b642bed6e5ed1df5aebfbd3b849c1c66a
Bravco <boss.wiw@gmail.com>
2023-06-07 09:17:50 +0200

    Vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -13,6 +13,15 @@ export default defineConfig({
     }
   },
   build: {
-    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: true,
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
