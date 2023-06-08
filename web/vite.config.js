e6117578b65b6b227798eb3e0fa345520640ea8a
Bravco <boss.wiw@gmail.com>
2023-06-08 06:01:21 +0200

    Changed base

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/training-lab/web/",
+  base: "/training-lab/",
   plugins: [vue()],
   resolve: {
     alias: {
